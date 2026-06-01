edba851e0cd674c37b29f0b2dd69253e33c05312
Michał Kucia <kucia.michal@gmail.com>
2026-06-01 11:53:28 +0200

    SPEC Lista update 01.06.2026 11:53:28,71

--- push_Lista.bat
+++ push_Lista.bat
@@ -1,6 +1,6 @@
 @echo off
 echo === Lista Obecnosci - Push do GitHub ===
-cd /d "%USERPROFILE%\Downloads\Lista"
+cd /d "%~dp0"
 git add .
 git commit -m "SPEC Lista update %date% %time%"
 git push origin master:main --force
